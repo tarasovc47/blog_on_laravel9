487f648aa6bbb42d55e343a82d0d275fcd8cca51
tarasovc47 <tarasovc47@live.ru>
2022-02-14 12:11:19 +0500

    выполнены миграции

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('login')->unique();
             $table->string('name');
+            $table->string('gender');
+            $table->string('birthday');
+            $table->boolean('isAdmin')->default(false);
+            $table->boolean('isBlocked')->default(false);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
